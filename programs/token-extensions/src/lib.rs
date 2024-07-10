a338dbac7b02817a60bceafb9994e610b1e7c5ab
timadigwe <timothyadigwe2019@gmail.com>
2024-07-10 16:34:28 +0100

    init

---
+++ programs/token-extensions/src/lib.rs
@@ -0,0 +1,30 @@
+use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod utils;
+pub use instructions::*;
+pub use utils::*;
+
+
+declare_id!("4sGXURvXXXZqxtcT4JHEoUwnfnVyxtSqBhvwnNvVnDPA");
+
+#[program]
+pub mod token_extensions {
+    use super::*;
+
+
+    pub fn create_mint_account(
+        ctx: Context<CreateMintAccount>,
+        args: CreateMintAccountArgs,
+    ) -> Result<()> {
+        instructions::handler(ctx, args)
+    }
+
+    
+    pub fn check_mint_extensions_constraints(
+        _ctx: Context<CheckMintExtensionConstraints>,
+    ) -> Result<()> {
+        Ok(())
+    }
+}
+
